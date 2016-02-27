2e3a124e4f8b7ba62107594cae9d06d911768433
Marcin Jasiński <mkjasinski@gmail.com>
2016-02-27 19:33:58 +0100

    Added Test class

---
+++ src/com/marcinjasinski/wsg/psio/l1/s2/Main.java
@@ -0,0 +1,53 @@
+package com.marcinjasinski.wsg.psio.l1.s2;
+
+import java.io.Serializable;
+
+/**
+ * Class Main.
+ *
+ * @author Marcin Jasiński {@literal <mkjasinski@gmail.com>}
+ */
+public class Main implements Serializable {
+
+    public static void main(String[] args) {
+        final Test test = new Test(2, 3, 2);
+
+        System.out.println(test);
+
+        test.e = 4;
+        test.f = 12;
+
+        System.out.println(test);
+
+        final Test test1 = new Test();
+
+        System.out.println(test1);
+
+        final Test test2 = new Test(3, 4);
+
+        System.out.println(test2);
+
+        final Test test3 = new Test();
+
+        System.out.println(test3);
+
+        test3.setC(11);
+
+        System.out.println(test3);
+
+        double sum = test3.sum();
+
+        System.out.println(sum);
+
+        test3.setSum(11);
+
+        System.out.println(test3.getSum());
+
+        //        while (true) {
+        //            Test test4 = new Test(1, 2, 3);
+        //            test4 = null;
+        //
+        //            System.gc();
+        //        }
+    }
+}
---
+++ src/com/marcinjasinski/wsg/psio/l1/s2/Test.java
@@ -0,0 +1,101 @@
+package com.marcinjasinski.wsg.psio.l1.s2;
+
+import java.io.Serializable;
+
+/**
+ * Class Test.
+ *
+ * @author Marcin Jasiński {@literal <mkjasinski@gmail.com>}
+ */
+public class Test implements Serializable {
+
+    public int e;
+    protected int g;
+    int f;
+    private int a;
+    private int b;
+    private double c;
+    private float d;
+    private double sum;
+
+    {
+        d = 10;
+    }
+
+    public Test() {
+    }
+
+    public Test(final int b, final double c) {
+        this(1, b, c);
+    }
+
+    public Test(final int a, final int b, final double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double sum() {
+        sum = a + b + c + d;
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+               "a=" + a +
+               ", b=" + b +
+               ", c=" + c +
+               ", d=" + d +
+               ", sum=" + sum +
+               ", e=" + e +
+               ", f=" + f +
+               ", g=" + g +
+               '}';
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Usuwam obiekt!");
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(final double c) {
+        this.c = c;
+    }
+
+    public float getD() {
+        return d;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(final double sum) {
+        this.sum = sum;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public int getF() {
+        return f;
+    }
+}
