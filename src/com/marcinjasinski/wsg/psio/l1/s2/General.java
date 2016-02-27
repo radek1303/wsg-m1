ce9d03b114d8f138352d247ca30b5c7539c3d2d9
Marcin Jasiński <mkjasinski@gmail.com>
2016-02-27 11:37:26 +0100

    Changed issues

--- src/com/marcinjasinski/wsg/psio/l1/s2/General.java
+++ src/com/marcinjasinski/wsg/psio/l1/s2/General.java
@@ -14,10 +14,10 @@ public class General implements Serializable {
         2.  modyfikatory dostępu
         3.  tworzenie instancji klas, wyjątki
         4.  metody i dane składowe
-        5.  uzyskanie dostępu do pól i metod
-        6.  przeciążanie, a nadpisywanie
-        7.  konstruktory (destruktory?), bloki inicjujące, static {}
-        8.  słowo kluczowe this
+        5.  konstruktory (destruktory?), bloki inicjujące, static {}
+        6.  słowo kluczowe this
+        7.  uzyskanie dostępu do pól i metod
+        8.  przeciążanie, a nadpisywanie
         9.  zmienna lista argumentów
         10. dziedziczenie, superklasy i podklasy
         11. interfejsy oraz wyliczenia, polimorfizm
