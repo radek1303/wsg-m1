e418a82c41061f197f7d2494b43acc218d9bd8b7
Marcin Jasiński <mkjasinski@gmail.com>
2016-02-28 03:34:23 +0100

    Added task

---
+++ src/com/marcinjasinski/wsg/psio/l1/s2/tasks/Pozycja.java
@@ -0,0 +1,44 @@
+package com.marcinjasinski.wsg.psio.l1.s2.tasks;
+
+import java.io.Serializable;
+
+/**
+ * Class Pozycja.
+ *
+ * @author Marcin Jasiński {@literal <mkjasinski@gmail.com>}
+ */
+public class Pozycja implements Serializable {
+
+    private String nazwaTowaru;
+    private int ileSztuk;
+    private double cena;
+
+    public Pozycja(final String nazwaTowaru, final int ileSztuk, final double cena) {
+        this.nazwaTowaru = nazwaTowaru;
+        this.ileSztuk = ileSztuk;
+        this.cena = cena;
+    }
+
+    public String getNazwaTowaru() {
+        return nazwaTowaru;
+    }
+
+    public int getIleSztuk() {
+        return ileSztuk;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    @Override
+    public String toString() {
+        return String.
+                format("%-20s %10.2f zł %4d szt. %10.2f zł", nazwaTowaru, cena, ileSztuk,
+                       obliczWartosc());
+    }
+
+    public double obliczWartosc() {
+        return ileSztuk * cena;
+    }
+}
---
+++ src/com/marcinjasinski/wsg/psio/l1/s2/tasks/Task1.java
@@ -0,0 +1,17 @@
+package com.marcinjasinski.wsg.psio.l1.s2.tasks;
+
+import java.io.Serializable;
+
+/**
+ * Class Task1.
+ *
+ * @author Marcin Jasiński {@literal <mkjasinski@gmail.com>}
+ */
+public class Task1 implements Serializable {
+
+    public static void main(String[] args) {
+        Pozycja pozycja = new Pozycja("Cytryny", 10, 23.4);
+
+        System.out.println(pozycja);
+    }
+}
